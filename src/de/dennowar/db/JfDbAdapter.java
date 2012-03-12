80fe75339c2af93ae2a7c2467efb6314f2fa9ce1
Dennis Schmidt <schmidde@fh-brandenburg.de>
2012-03-12 11:16:12 +0100

    DB um Status erweitert

--- src/de/dennowar/db/JfDbAdapter.java
+++ src/de/dennowar/db/JfDbAdapter.java
@@ -14,17 +14,19 @@ public class JfDbAdapter {
 	public final static String UID = "_uid";
 	public final static String EMAIL = "email";
 	public final static String PASSWD = "passwd";
+	public final static String STATE = "state";
 
 	private final static String TAG = "JfDbAdapter";
 	private final static String DB_NAME = "jfDB";
 	private final static String DB_TABLE = "user";
-	private final static int DB_VERSION = 1;
+	private final static int DB_VERSION = 2;
 	
 	//String erzeugt SQLite Tabelle
 	public static final String DB_CREATE = "create table "+ DB_TABLE + "( " +
 			UID + " integer primary key autoincrement, " +
 			EMAIL + " text not null, " +
-			PASSWD + " text not null);";
+			PASSWD + " text not null, " +
+			STATE + " integer not null);";
 	
 	private DatabaseHelper mDbHelper;
 	private SQLiteDatabase mDb;
@@ -65,26 +67,42 @@ public class JfDbAdapter {
 		mDb.close();
 	}
 	
-	public long create(String email, String passwd){
+	public long create(String email, String passwd, int state){
 		
 		ContentValues cv = new ContentValues();
 		cv.put(EMAIL, email);
 		cv.put(PASSWD, passwd);
+		cv.put(STATE, state);
 		return mDb.insert(DB_TABLE, null, cv);
 	}
 	
 	public Cursor fetchAllUser(){
-		Cursor cur = mDb.query(DB_TABLE, new String[]{UID, EMAIL, PASSWD}, null, null, null, null, null, null);
+		Cursor cur = mDb.query(DB_TABLE, new String[]{UID, EMAIL, PASSWD, STATE}, null, null, null, null, null, null);
 		if(cur != null){
 			cur.moveToFirst();
 		}
 		return cur;
 	}
+	
 	public Cursor fetchUser(String email){
-		Cursor cur = mDb.query(true, DB_TABLE, new String[]{UID, EMAIL, PASSWD}, EMAIL + " = '" + email + "'", null, null, null, null, null);
+		Cursor cur = mDb.query(true, DB_TABLE, new String[]{UID, EMAIL, PASSWD, STATE}, EMAIL + " = '" + email + "'", null, null, null, null, null);
 		if(cur != null){
 			cur.moveToFirst();
 		}
 		return cur;
 	}
+	
+	public boolean updateUser(long uid, String email, String passwd, int state) {
+        ContentValues args = new ContentValues();
+        args.put(EMAIL, email);
+        args.put(PASSWD, passwd);
+        args.put(STATE, state);
+
+        return mDb.update(DB_TABLE, args, UID + "='" + uid + "'", null) > 0;
+    }
+	
+	public boolean deleteNote(long uid) {
+
+        return mDb.delete(DB_TABLE, UID + "='" + uid + "'", null) > 0;
+    }
 }
